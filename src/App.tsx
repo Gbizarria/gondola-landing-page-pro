5bcd76246b60ff80ec33ce46fa5e821e960121c5
Gustavo Bizarria <Gustavobizarria@icloud.com>
2025-05-14 20:14:35 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/gondola-landing-page-pro" element={<Index />} />
+          <Route path="/gondola-landing-page-pro/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
